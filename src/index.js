6954b22958104811ef2da40e63621fa1d18115a9
NamTaeHo <han127kr@gmail.com>
2024-05-08 18:03:05 +0900

    Node js 모듈 시스템

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import mul from "./math.js";
+import { add, sub } from "./math.js";
+
+console.log("안녕 node js");
+
+console.log(add(2, 3));
+console.log(sub(2, 3));
+console.log(mul(2, 3));
---
+++ src/math.js
@@ -0,0 +1,20 @@
+//math
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+export default function multifly(a, b) {
+  return a * b;
+}
+
+// module.exports = {
+//   add: add,
+//   sub: sub,
+// };
+
+// export { add, sub };
