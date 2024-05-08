1538ad21044e000c90a3d404ce2dc287c6b5c7e7
NamTaeHo <han127kr@gmail.com>
2024-05-08 19:35:01 +0900

    node js 라이브러리 사용

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import mul from "./math.js";
 import { add, sub } from "./math.js";
 
-console.log("안녕 node js");
+import randomColor from "randomcolor";
 
-console.log(add(2, 3));
-console.log(sub(2, 3));
-console.log(mul(2, 3));
+const color = randomColor();
+console.log(color);
+
+// console.log("안녕 node js");
+
+// console.log(add(2, 3));
+// console.log(sub(2, 3));
+// console.log(mul(2, 3));
